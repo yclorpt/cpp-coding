ff160ac43751c4ae32e7047d855bb38b13b794be
yclorpt <tdaniels058@gmail.com>
2025-10-01 01:05:15 -0600

    getting things ready

---
+++ bookstore.cpp
@@ -0,0 +1,21 @@
+// Store keeps a file of transactions. Each records the sale of 1+ copies of a single book
+// 3 Data elements:
+//   0-201-70353-X : ISBN/Book Identifier
+//   4             : # of Copies Sold
+//   24.99         : Price
+//
+// Each book needs:
+//   # of Copies Sold
+//   Total revenue from book
+//   Average sales price
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  cout << "Hello World" << endl;
+
+  return 0;
+}
---
+++ build/CMakeFiles/bookstore.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bookstore.dir/link.d"
+  "CMakeFiles/bookstore.dir/bookstore.cpp.o"
+  "CMakeFiles/bookstore.dir/bookstore.cpp.o.d"
+  "bookstore"
+  "bookstore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bookstore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
